78208e962976fa31e16b6bc9c63709ce9f425a80
Sindreedelange <146169@stud.hvl.no>
2018-09-21 00:19:26 +0200

    Cleanup

--- Project1-Implementing_Decision_Trees/classes/DataCleaning.py
+++ Project1-Implementing_Decision_Trees/classes/DataCleaning.py
@@ -2,6 +2,7 @@
 
 import pandas as pd
 import numpy as np
+from sklearn.model_selection import train_test_split
 
 class DataCleaning(object):
 
@@ -14,7 +15,13 @@ class DataCleaning(object):
 
     def factorizeDf(self, dataframe):
         """ Factorizes inputed dataframe, so that its data is categorical, discrete values"""
+        pd.options.mode.chained_assignment = None
         data_factorized = dataframe
         for column in dataframe:
             data_factorized[column], unique_data = pd.factorize(dataframe[column])
         return data_factorized
+
+    def train_test_prune(self, X, y, test, prune):
+        X_train, X_test, y_train, y_test= train_test_split(X, y, test_size=test, random_state=42, straify=y)
+        X_train, X_prune, y_train, y_prune = train_test_split(X_train, y_train, test_size=prune, random_state=42, stratify=y_train)
+        return X_train, X_prune, X_test, y_train, y_prune, y_test 
