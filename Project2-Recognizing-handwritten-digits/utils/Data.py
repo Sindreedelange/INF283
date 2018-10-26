ca649e59c1c85450d33956ebe216acdf9cd2ecef
Sindreedelange <146169@stud.hvl.no>
2018-10-26 22:47:13 +0200

    Follow conventions

--- Project2-Recognizing-handwritten-digits/utils/Data.py
+++ Project2-Recognizing-handwritten-digits/utils/Data.py
@@ -19,11 +19,11 @@ class Data(object):
         y_test_cat = np_utils.to_categorical(y_test)
         return y_train_cat, y_val_cat, y_test_cat
 
-    def print_data_shapes_val(self, X_train, X_test, y_train, y_test, y_val = None, X_val = None):
-        print("X_train: ", X_train.shape)
-        if X_val is not None:
-            print("X_val: ", X_val.shape)
-        print("X_test: ", X_test.shape)
+    def print_data_shapes_val(self, x_train, x_test, y_train, y_test, y_val=None, x_val=None):
+        print("X_train: ", x_train.shape)
+        if x_val is not None:
+            print("X_val: ", x_val.shape)
+        print("X_test: ", x_test.shape)
         print("y_train: ", y_train.shape)
         if y_val is not None:
             print("y_val: ", y_val.shape)
@@ -36,23 +36,23 @@ class Data(object):
         labels_path = file_list[1]
         images_path_full = os.path.join(DATA_PATH + images_path)
         labels_path_full = os.path.join(DATA_PATH + labels_path)
-        X = pd.read_csv(images_path_full)
+        x = pd.read_csv(images_path_full)
         if reshape:
-            X = X.values.astype('float32')
+            x = x.values.astype('float32')
             # Normalize data
-            X = X / 255
+            x = x / 255
             # Reshape for cnn
-            X = X.reshape([-1, 28, 28, 1]).astype('float32')
+            x = x.reshape([-1, 28, 28, 1]).astype('float32')
         else:
             # Normalize data
-            X = X / 255
+            x = x / 255
         y = pd.read_csv(labels_path_full)
         # Encode labels
         if one_hot_enc:
             encoder = OneHotEncoder(sparse=False, categories='auto')
             y = encoder.fit_transform(y)
         # Divide into train and test set
-        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=self.random_state)
+        X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=self.random_state)
         if train_val_test:
             X_train, X_val, y_train, y_val = train_test_split(X_train, y_train, test_size=0.3,
                                                               random_state=self.random_state)
