b9b7977cf419be886697092c6ed048fc5c9427bb
Sindreedelange <146169@stud.hvl.no>
2018-10-26 18:06:17 +0200

    Main init - make it possible for the user to print out the results from the 4 candidate models

---
+++ Project2-Recognizing-handwritten-digits/Main.py
@@ -0,0 +1,21 @@
+import numpy as np
+
+
+def load_result_dicts():
+    classifiers_dict = np.load("dictionaries/classifiers_dict.npy").item()
+    optim_classifier_dict = np.load("dictionaries/optim_classifier_dict.npy").item()
+    optim_classifier_cnn_params = np.load("dictionaries/optim_classifier_cnn_params.npy").item()
+    return classifiers_dict, optim_classifier_dict, optim_classifier_cnn_params
+
+
+class Main:
+    classifiers_dict, optim_classifier_dict, optim_classifier_cnn_params = load_result_dicts()
+    print("Scores for the 4 candidate models: \n", classifiers_dict)
+    print("\n Scores for the optimal, chosen end model (CNN) after running"
+          "custom GridSearch: \n", optim_classifier_dict)
+    print("\n Using the following parameters: \n", optim_classifier_cnn_params)
+
+
+
+
+
